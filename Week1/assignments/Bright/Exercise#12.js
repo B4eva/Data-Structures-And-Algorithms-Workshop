e22cd8a2200bb7cef8780be87b238757d0af1580
DELL <tanzebright@gmail.com>
2020-05-22 05:29:40 +0100

    palindrome test

---
+++ Week1/assignments/Bright/Exercise#12.js
@@ -0,0 +1,31 @@
+ function palindrome(str) {
+
+    var lent = str.length;
+    var mid = Math.floor(lent/2);   //Round lent downward to its nearest integer:
+
+        // 
+    for ( var i = 0; i < mid; i++ ) {
+        if (str[i] !== str[lent - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+// test case
+console.log(palindrome("mom")); 
+
+
+
+
+
+/* function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+ var compare = reverseString(""); 
+if (compare == "str"){
+    console.log("true"); 
+} else {
+    console.log("false");
+}
+ */
